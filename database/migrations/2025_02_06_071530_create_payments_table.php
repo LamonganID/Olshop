937f542224ecf264903ce1a382da961a648e75a7
Your Name <you@example.com>
2025-02-06 14:51:51 +0700

    creat and config migration and models

---
+++ database/migrations/2025_02_06_071530_create_payments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreginId('order_id')->constrained()->cascadeOnDelete();
+            $table->enum('payment_method',['bank_transfer','e-wallet','cod']);
+            $table->enum('status',['pending','pending','sucsess','cancel']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
---
+++ database/migrations/2025_02_06_071552_create_reviews_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->casecadeOnDelete();
+            $table->foreignId('product_id')->constrained()->casecadeOnDelete();
+            $table->interger('ratting')->check('rating >= And rating <=5');
+            $table->text('review');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
