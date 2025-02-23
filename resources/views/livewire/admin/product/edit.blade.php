42d5ed889dde0f07ece674bb253246c1b78e943f
Your Name <you@example.com>
2025-02-23 16:34:23 +0700

    Add layout extension to product edit view and create migration for users table

--- resources/views/livewire/admin/product/edit.blade.php
+++ resources/views/livewire/admin/product/edit.blade.php
@@ -1,3 +1,5 @@
+@extends('layouts.app')
+@section('content')
 <div class="container mx-auto px-4 py-6">
     <h1 class="text-2xl font-bold mb-6">Edit Product</h1>
     <form wire:submit.prevent="updateProduct">
@@ -31,3 +33,4 @@
         </div>
     </form>
 </div>
+@endsection
